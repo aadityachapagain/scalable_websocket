39bef505a6a24b9e4882c71a6a470d36652d6276
Aaditya chapagain <chapagain.aaditya@gmail.com>
2020-07-24 19:49:32 +0545

    * add resource clean up callback * whoops i did it again! * back to the future past * add msg timeout ticks * check msg timeout ticks every 0.08 sec

--- client.py
+++ client.py
@@ -3,9 +3,11 @@ from tornado import gen
 from tornado.websocket import websocket_connect
 import uuid
 import random
+import asyncio
 import time
-from threading import Thread
 import traceback
+import gc
+from threading import Thread
 
 def _get_id():
     return str(uuid.uuid4())
@@ -18,19 +20,39 @@ def select_rand_socket(socs):
         soc.out_state.append('rand msg: {:.3f}'.format(random.random()))
         time.sleep(random.randint(1,8))
 
+def clean_client_timeout_fn(_id):
+    for client in clients:
+        if client.soc_id == _id:
+            del client
+            break
+    # after deleting the conn object and socket 
+    # garbage collect all the resources
+    gc.collect()
+
 class Client(object):   
-    def __init__(self, url, timeout, signal_ping: False):
+    def __init__(self, url, timeout, conn_timeout_callback, soc_id, signal_ping: False):
         self.url = url
+        self.soc_id = soc_id
         self.timeout = timeout
-        # self.ioloop = IOLoop.current()
+        # set timeout callback to close conn and stop async loop
+        self._conn_timeout_callback = conn_timeout_callback
+        # recived msg from server
         self.state = []
+        # message to send to server
         self.out_state = []
+
+        # timeout_counter
+        self.timeout_counter = 0
         self.ws = None
         self.connect()
-        PeriodicCallback(self.send_new_message, 1000).start()
+        # check for new message every 0.1 sec
+        self.mesg_check_callback = PeriodicCallback(self.send_new_message, 800)
+        self.mesg_check_callback.start()
+
+        self.signal_ping = signal_ping
         if signal_ping:
-            PeriodicCallback(self.keep_alive, 20000).start()
-        # self.ioloop.start()
+            self.ping_callback = PeriodicCallback(self.keep_alive, 20000)
+            self.ping_callback.start()
 
     @gen.coroutine
     def connect(self):
@@ -43,7 +65,6 @@ class Client(object):
         else:
             print ("connected")
             self.run()
-            # self.send_new_message()
 
     @gen.coroutine
     def run(self):
@@ -62,18 +83,24 @@ class Client(object):
             self.ws.write_message("keep alive")
 
     def close_conn(self):
+        self.mesg_check_callback.stop()
+        if self.signal_ping:
+            self.ping_callback.stop()
         self.ws.close()
+        if self._conn_timeout_callback:
+            self._conn_timeout_callback(self.soc_id)
 
-    # @gen.coroutine
     def send_new_message(self):
-        # while True:
         if self.ws is None:
             return
-        if len(self.out_state) > 0:
-            self.send_message(self.out_state.pop(0))
-
-    def get_new_message(self):
-        return 
+        if self.timeout_counter > self.timeout:
+            pass
+        else:
+            if len(self.out_state) > 0:
+                self.timeout_counter = 0
+                self.send_message(self.out_state.pop(0))
+            else:
+                self.timeout_counter+= 1
 
     def send_message(self, message):
         if self.ws is None:
@@ -83,7 +110,7 @@ class Client(object):
 
 if __name__ == "__main__":
     port = 10001
-    clients = [Client("ws://localhost:{}/websocket".format(10001), 5, True) for i in range(10)]
+    clients = [Client("ws://localhost:{}/websocket".format(10001), 5000, clean_client_timeout_fn,_get_id() ,True) for i in range(10)]
     Thread(target = select_rand_socket, args= (clients, )).start()
     ioloop = IOLoop.current()
     ioloop.start()
