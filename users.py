b318d6da7d066978e2e2a8ad2c095aeb05476270
Aaditya chapagain <chapagain.aaditya@gmail.com>
2020-07-26 18:38:51 +0545

    user db

---
+++ users.py
@@ -0,0 +1,10 @@
+users_db = {
+    "embodied": {
+        "username": "embodied",
+        "email": "stefen@embodied.me",
+        "hashed_password": "$2b$12$4pX4UtOi3Vqv1MOsDz0BteS8tFEqdXoSpiSAR4s4gjN4fVCgUAOy6",
+        "disabled": False,
+    },
+}
+
+__password = "6CFembodied#12$"
